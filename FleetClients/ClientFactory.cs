29a1ea9a94ecb38ef1c6fc6c0c424b6092d4c917
Martin Davies <martin.davies@guidanceautomation.com>
2018-08-17 15:36:19 +0100

    Adds fleet client

---
+++ FleetClients/ClientFactory.cs
@@ -0,0 +1,12 @@
+﻿using BaseClients;
+
+namespace FleetClients
+{
+    public static class ClientFactory
+    {
+        public static IFleetManagerClient CreateTcpFleetManagerClient(EndpointSettings portSettings)
+        {
+            return new FleetManagerClient(portSettings.TcpFleetManagerService());
+        }
+    }
+}
