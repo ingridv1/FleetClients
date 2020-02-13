9bf32438ddb92baf32e30ee3fdaecbaa6615c553
Martin Davies <martin.davies@guidanceautomation.com>
2020-02-13 12:09:01 +0000

    Web browser navigation

--- FleetClients.DemoApp/ViewModel/TutorialViewModel.cs
+++ FleetClients.DemoApp/ViewModel/TutorialViewModel.cs
@@ -3,8 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
 using FleetClients.DemoApp.Model;
 using GACore;
+using GACore.Command;
+using Markdig;
 
 namespace FleetClients.DemoApp.ViewModel
 {
@@ -12,6 +16,30 @@ namespace FleetClients.DemoApp.ViewModel
 	{
 		internal TutorialViewModel()
 		{
+			HandleLoadCommands();
+		}
+		public ICommand RequestNavigateCommand { get; set; }
+
+		private void HandleLoadCommands()
+		{
+			RequestNavigateCommand = new CustomCommand(RequestNavigate, CanRequestNavigate);
+		}
+
+		private bool CanRequestNavigate(object obj) => true;
+
+		private void RequestNavigate(object obj)
+		{
+			try
+			{
+				string html = Markdown.ToHtml(Properties.Resources.MainWindowDescription);
+
+				WebBrowser webBrowser = (WebBrowser)obj;
+				webBrowser.NavigateToString(html);
+			}
+			catch (Exception ex)
+			{
+				Logger.Error(ex);
+			}
 		}
 	}
 }
