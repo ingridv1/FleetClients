45d5e2938fa29fd549408eda094ed2597fe01b0e
Martin Davies <martin.davies@guidanceautomation.com>
2020-02-19 13:50:54 +0000

    Binding nicely

--- FleetClients.UI/ViewModel/KingpinStateMailboxViewModel.cs
+++ FleetClients.UI/ViewModel/KingpinStateMailboxViewModel.cs
@@ -1,14 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using GACore;
+﻿using GACore;
 using GACore.Architecture;
 
 namespace FleetClients.UI.ViewModel
 {
 	public class KingpinStateMailboxViewModel : AbstractViewModel<KingpinStateMailbox>
 	{
+		private IKingpinState kingpinState = null;
+
+		public IKingpinState KingpinState
+		{
+			get { return kingpinState; }
+			private set
+			{
+				if (kingpinState != value)
+				{
+					kingpinState = value;
+					OnNotifyPropertyChanged();
+				}
+			}
+		}
+
+		protected override void HandleModelUpdate(KingpinStateMailbox oldValue, KingpinStateMailbox newValue)
+		{
+			if (oldValue != null) oldValue.Updated -= Model_Updated;		
+			if (newValue != null) newValue.Updated += Model_Updated;
+
+			KingpinState = (newValue != null) ? newValue.Mail : null;
+
+			base.HandleModelUpdate(oldValue, newValue);
+		}
+
+		private void Model_Updated()
+		{
+			KingpinState = Model?.Mail;
+		}
 	}
 }
--- FleetClients.UI/ViewModel/KingpinStatesViewModel.cs
+++ FleetClients.UI/ViewModel/KingpinStatesViewModel.cs
@@ -3,13 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using GACore;
 using GACore.Architecture;
+using GACore.Command;
 
 namespace FleetClients.UI.ViewModel
 {
 	public class KingpinStatesViewModel : AbstractCollectionViewModel<IFleetManagerClient, KingpinStateMailboxViewModel, KingpinStateMailbox>
 	{
+
+		public KingpinStatesViewModel()			
+		{
+			HandleLoadCommands();
+		}
+
+		public ICommand SelectedKingpinCommand { get; set; }
+
+		private void HandleLoadCommands()
+		{
+			SelectedKingpinCommand = new CustomCommand(SelectedKingpin, CanSelectedKingpin);
+		}
+
+		private void SelectedKingpin(object obj)
+		{
+			try
+			{
+				KingpinStateMailboxViewModel ksmViewModel = (KingpinStateMailboxViewModel)obj;
+			}
+			catch(Exception ex)
+			{
+				Logger.Error(ex);
+			}
+		}
+
+		private bool CanSelectedKingpin(object obj) => true;
+
 		public override KingpinStateMailboxViewModel GetViewModelForModel(KingpinStateMailbox model)
 		{
 			throw new NotImplementedException();
