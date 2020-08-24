1df64611bdf68cf63202b5ae6c4c8fea9d5e5d39
Martin Davies <martin.davies@guidanceautomation.com>
2020-08-24 11:55:01 +0100

    Update FleetManagerClient.cs
    Refactors to use generic handlers

--- src/FleetClients.Core/FleetManagerClient.cs
+++ src/FleetClients.Core/FleetManagerClient.cs
@@ -10,6 +10,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Reflection;
 using System.ServiceModel;
 using System.Xml.Linq;
 
@@ -17,13 +18,15 @@ namespace FleetClients.Core
 {
     internal class FleetManagerClient : AbstractCallbackClient<IFleetManagerService_PublicAPI_v2_0>, IFleetManagerClient
     {
-        private readonly FleetManagerServiceCallback callback = new FleetManagerServiceCallback();
+        private readonly Queue<byte[]> buffer = new Queue<byte[]>();
 
-        private bool isDisposed = false;
+        private readonly FleetManagerServiceCallback callback = new FleetManagerServiceCallback();
 
         private readonly List<KingpinStateMailbox> kingpinStateMailboxes = new List<KingpinStateMailbox>();
 
-        private readonly Queue<byte[]> buffer = new Queue<byte[]>();
+        private FleetState fleetState = null;
+
+        private bool isDisposed = false;
 
         /// <summary>
         /// Creates a new FleetManagerClient
@@ -36,67 +39,103 @@ namespace FleetClients.Core
             callback.FleetStateUpdate += Callback_FleetStateUpdate;
         }
 
-        private void Callback_FleetStateUpdate(FleetState fleetState)
-        {
-            FleetState = fleetState;
-
-            lock (kingpinStateMailboxes)
-            {
-                foreach (IKingpinState kingpinState in fleetState.KingpinStates)
-                {
-                    KingpinStateMailbox mailbox = kingpinStateMailboxes.FirstOrDefault(e => e.Key.Equals(kingpinState.IPAddress));
-
-                    if (mailbox != null)
-                        mailbox.Update(kingpinState);
-                    else
-                    {
-                        KingpinStateMailbox mailBox = new KingpinStateMailbox(kingpinState.IPAddress, kingpinState);
-
-                        kingpinStateMailboxes.Add(mailBox);
-                        OnAdded(mailBox);
-                    }
-                }
-
-                IEnumerable<IPAddress> activeIP = fleetState.KingpinStates.Select(e => e.IPAddress);
-                IEnumerable<IPAddress> deadIP = kingpinStateMailboxes.Select(e => e.Key).Except(activeIP).ToList();
-
-                foreach (IPAddress ipAddress in deadIP)
-                {
-                    KingpinStateMailbox deadMailBox = kingpinStateMailboxes.First(e => e.Key == ipAddress);
-                    kingpinStateMailboxes.Remove(deadMailBox);
-
-                    OnRemoved(deadMailBox);
-                }
-            }
-        }
-
-        public TimeSpan Heartbeat { get; private set; }
-
         ~FleetManagerClient()
         {
             Dispose(false);
         }
 
+        public event Action<KingpinStateMailbox> Added;
+
+        public event Action<KingpinStateMailbox> Removed;
+
+        public FleetState FleetState
+        {
+            get { return fleetState; }
+            set
+            {
+                if (fleetState == null || value.Tick.IsCurrentByteTickLarger(fleetState.Tick))
+                    fleetState = value;
+            }
+        }
+
+        public TimeSpan Heartbeat { get; private set; }
+
+        public IServiceCallResult CreateVirtualVehicle(IPAddress ipAddress, PoseDto pose)
+        {
+            Logger.Trace("CreateVirtualVehicle");
+            return HandleAPICall(e => e.CreateVirtualVehicle(ipAddress, pose));
+        }
+
+        public IServiceCallResult<SemVerDto> GetAPISemVer()
+        {
+            Logger.Trace("GetAPISemVer");
+            return HandleAPICall<SemVerDto>(e => e.GetAPISemVer());
+        }
+          
+
         public IServiceCallResult<XElement> GetKingpinDescription(IPAddress ipAddress)
         {
-            Logger.Trace("GetKingpinDescription()");
+            Logger.Trace("GetKingpinDescription");
+            return HandleAPICall<XElement>(e => e.GetKingpinDescription(ipAddress));
+        }
 
-            try
-            {
-                using (ChannelFactory<IFleetManagerService_PublicAPI_v2_0> channelFactory = CreateChannelFactory())
-                {
-                    IFleetManagerService_PublicAPI_v2_0 channel = channelFactory.CreateChannel();
-                    ServiceCallResultDto<XElement> result = channel.GetKingpinDescription(ipAddress);
-                    channelFactory.Close();
+        public IEnumerable<KingpinStateMailbox> GetModels() => kingpinStateMailboxes.ToList();
 
-                    return result;
-                }
-            }
-            catch (Exception ex)
-            {
-                Logger.Error(ex);
-                return ServiceCallResultFactory<XElement>.FromClientException(ex);
-            }
+        public IServiceCallResult RemoveVehicle(IPAddress ipAddress)
+        {
+            Logger.Trace("RemoveVehicle");
+            return HandleAPICall(e => e.RemoveVehicle(ipAddress));
+        }
+
+        public IServiceCallResult RequestFreeze()
+        {
+            Logger.Trace("RequestFreeze");
+            return HandleAPICall(e => e.RequestFreeze());
+        }
+
+        public IServiceCallResult RequestUnfreeze()
+        {
+            Logger.Trace("RequestUnfreeze");
+            return HandleAPICall(e => e.RequestUnfreeze());
+        }
+
+        public IServiceCallResult ResetKingpin(IPAddress ipAddress)
+        {
+            Logger.Trace("ResetKingpin");
+            return HandleAPICall(e => e.ResetKingpin(ipAddress));
+        }
+
+        public IServiceCallResult SetFleetState(VehicleControllerState controllerState)
+        {
+            Logger.Trace("SetFleetState");
+            return HandleAPICall(e => e.SetFleetState(controllerState));
+        }
+
+        public IServiceCallResult SetKingpinState(IPAddress ipAddress, VehicleControllerState controllerState)
+        {
+            Logger.Trace("SetKingpinState");
+            return HandleAPICall(e => e.SetKingpinState(ipAddress, controllerState));
+        }
+
+        public IServiceCallResult SetPose(IPAddress ipAddress, PoseDto pose)
+        {
+            Logger.Trace("SetPose");
+            return HandleAPICall(e => e.SetPose(ipAddress, pose));
+        }
+
+        protected override void Dispose(bool isDisposing)
+        {
+            Logger.Debug($"Dispose isDisposing:{isDisposing}");
+
+            if (isDisposed)
+                return;
+
+            if (isDisposing)
+                callback.FleetStateUpdate -= Callback_FleetStateUpdate;
+
+            base.Dispose(isDisposing);
+
+            isDisposed = true;
         }
 
         protected override void HeartbeatThread()
@@ -150,20 +189,86 @@ namespace FleetClients.Core
             context = new InstanceContext(this.callback);
         }
 
-        private FleetState fleetState = null;
-
-        public event Action<KingpinStateMailbox> Added;
-
-        public event Action<KingpinStateMailbox> Removed;
-
-        private void OnRemoved(KingpinStateMailbox kingpinStateMailbox)
+        private void Callback_FleetStateUpdate(FleetState fleetState)
         {
-            Action<KingpinStateMailbox> handlers = Removed;
+            FleetState = fleetState;
 
-            handlers?
-                .GetInvocationList()
-                .Cast<Action<KingpinStateMailbox>>()
-                .ForEach(e => e.BeginInvoke(kingpinStateMailbox, null, null));
+            lock (kingpinStateMailboxes)
+            {
+                foreach (IKingpinState kingpinState in fleetState.KingpinStates)
+                {
+                    KingpinStateMailbox mailbox = kingpinStateMailboxes.FirstOrDefault(e => e.Key.Equals(kingpinState.IPAddress));
+
+                    if (mailbox != null)
+                        mailbox.Update(kingpinState);
+                    else
+                    {
+                        KingpinStateMailbox mailBox = new KingpinStateMailbox(kingpinState.IPAddress, kingpinState);
+
+                        kingpinStateMailboxes.Add(mailBox);
+                        OnAdded(mailBox);
+                    }
+                }
+
+                IEnumerable<IPAddress> activeIP = fleetState.KingpinStates.Select(e => e.IPAddress);
+                IEnumerable<IPAddress> deadIP = kingpinStateMailboxes.Select(e => e.Key).Except(activeIP).ToList();
+
+                foreach (IPAddress ipAddress in deadIP)
+                {
+                    KingpinStateMailbox deadMailBox = kingpinStateMailboxes.First(e => e.Key == ipAddress);
+                    kingpinStateMailboxes.Remove(deadMailBox);
+
+                    OnRemoved(deadMailBox);
+                }
+            }
+        }
+        /// <summary>
+        /// Handles an API call that returns an IServiceCallResult
+        /// </summary>
+        /// <param name="apiCall">Method the handles the channel call</param>
+        private IServiceCallResult HandleAPICall(Func<IFleetManagerService_PublicAPI_v2_0, ServiceCallResultDto> apiCall)
+        {
+            try
+            {
+                using (ChannelFactory<IFleetManagerService_PublicAPI_v2_0> channelFactory = CreateChannelFactory())
+                {
+                    IFleetManagerService_PublicAPI_v2_0 channel = channelFactory.CreateChannel();
+                    ServiceCallResultDto result = apiCall(channel);
+                    channelFactory.Close();
+
+                    return result;
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.Error(ex);
+                return ServiceCallResultFactory.FromClientException(ex);
+            }
+        }
+
+        /// <summary>
+        /// Handles an API call that returns a value of type T
+        /// </summary>
+        /// <typeparam name="T">Dto type to be returned</typeparam>
+        /// <param name="apiCall">Method the handles the channel call</param>
+        private IServiceCallResult<T> HandleAPICall<T>(Func<IFleetManagerService_PublicAPI_v2_0, ServiceCallResultDto<T>> apiCall)
+        {
+            try
+            {
+                using (ChannelFactory<IFleetManagerService_PublicAPI_v2_0> channelFactory = CreateChannelFactory())
+                {
+                    IFleetManagerService_PublicAPI_v2_0 channel = channelFactory.CreateChannel();
+                    ServiceCallResultDto<T> result = apiCall(channel);
+                    channelFactory.Close();
+
+                    return result;
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.Error(ex);
+                return ServiceCallResultFactory<T>.FromClientException(ex);
+            }
         }
 
         private void OnAdded(KingpinStateMailbox kingpinStateMailbox)
@@ -176,229 +281,14 @@ namespace FleetClients.Core
                 .ForEach(e => e.BeginInvoke(kingpinStateMailbox, null, null));
         }
 
-        public FleetState FleetState
+        private void OnRemoved(KingpinStateMailbox kingpinStateMailbox)
         {
-            get { return fleetState; }
-            set
-            {
-                if (fleetState == null || value.Tick.IsCurrentByteTickLarger(fleetState.Tick))
-                    fleetState = value;
-            }
+            Action<KingpinStateMailbox> handlers = Removed;
+
+            handlers?
+                .GetInvocationList()
+                .Cast<Action<KingpinStateMailbox>>()
+                .ForEach(e => e.BeginInvoke(kingpinStateMailbox, null, null));
         }
-
-        public IServiceCallResult RequestFreeze()
-        {
-            Logger.Trace("RequestFreeze()");
-
-            try
-            {
-                using (ChannelFactory<IFleetManagerService_PublicAPI_v2_0> channelFactory = CreateChannelFactory())
-                {
-                    IFleetManagerService_PublicAPI_v2_0 channel = channelFactory.CreateChannel();
-                    ServiceCallResultDto result = channel.RequestFreeze();
-                    channelFactory.Close();
-
-                    return result;
-                }
-            }
-            catch (Exception ex)
-            {
-                Logger.Error(ex);
-                return ServiceCallResultFactory.FromClientException(ex);
-            }
-        }
-
-        public IServiceCallResult<SemVerDto> GetAPISemVer()
-        {
-            Logger.Trace("GetSemVer()");
-
-            try
-            {
-                using (ChannelFactory<IFleetManagerService_PublicAPI_v2_0> channelFactory = CreateChannelFactory())
-                {
-                    IFleetManagerService_PublicAPI_v2_0 channel = channelFactory.CreateChannel();
-                    ServiceCallResultDto<SemVerDto> result = channel.GetAPISemVer();
-                    channelFactory.Close();
-
-                    return result;
-                }
-            }
-            catch (Exception ex)
-            {
-                Logger.Error(ex);
-                return ServiceCallResultFactory<SemVerDto>.FromClientException(ex);
-            }
-        }
-
-        public IServiceCallResult RequestUnfreeze()
-        {
-            Logger.Trace("TryRequestUnfreeze()");
-
-            try
-            {
-                using (ChannelFactory<IFleetManagerService_PublicAPI_v2_0> channelFactory = CreateChannelFactory())
-                {
-                    IFleetManagerService_PublicAPI_v2_0 channel = channelFactory.CreateChannel();
-                    ServiceCallResultDto result = channel.RequestUnfreeze();
-                    channelFactory.Close();
-
-                    return result;
-                }
-            }
-            catch (Exception ex)
-            {
-                Logger.Error(ex);
-                return ServiceCallResultFactory<XElement>.FromClientException(ex);
-            }
-        }
-
-        public IServiceCallResult CreateVirtualVehicle(IPAddress ipAddress, PoseDto pose)
-        {
-            Logger.Trace("TryCreateVirtualVehicle()");
-
-            try
-            {
-                using (ChannelFactory<IFleetManagerService_PublicAPI_v2_0> channelFactory = CreateChannelFactory())
-                {
-                    IFleetManagerService_PublicAPI_v2_0 channel = channelFactory.CreateChannel();
-                    ServiceCallResultDto result = channel.CreateVirtualVehicle(ipAddress, pose);
-                    channelFactory.Close();
-
-                    return result;
-                }
-            }
-            catch (Exception ex)
-            {
-                Logger.Error(ex);
-                return ServiceCallResultFactory.FromClientException(ex);
-            }
-        }
-
-        public IServiceCallResult RemoveVehicle(IPAddress ipAddress)
-        {
-            Logger.Trace("RemoveVehicle()");
-
-            try
-            {
-                using (ChannelFactory<IFleetManagerService_PublicAPI_v2_0> channelFactory = CreateChannelFactory())
-                {
-                    IFleetManagerService_PublicAPI_v2_0 channel = channelFactory.CreateChannel();
-                    ServiceCallResultDto result = channel.RemoveVehicle(ipAddress);
-                    channelFactory.Close();
-
-                    return result;
-                }
-            }
-            catch (Exception ex)
-            {
-                Logger.Error(ex);
-                return ServiceCallResultFactory.FromClientException(ex);
-            }
-        }
-
-        public IServiceCallResult SetPose(IPAddress ipAddress, PoseDto pose)
-        {
-            Logger.Trace("SetPose()");
-
-            try
-            {
-                using (ChannelFactory<IFleetManagerService_PublicAPI_v2_0> channelFactory = CreateChannelFactory())
-                {
-                    IFleetManagerService_PublicAPI_v2_0 channel = channelFactory.CreateChannel();
-                    ServiceCallResultDto result = channel.SetPose(ipAddress, pose);
-                    channelFactory.Close();
-
-                    return result;
-                }
-            }
-            catch (Exception ex)
-            {
-                Logger.Error(ex);
-                return ServiceCallResultFactory.FromClientException(ex);
-            }
-        }
-
-        public IServiceCallResult ResetKingpin(IPAddress ipAddress)
-        {
-            Logger.Trace("ResetKingpin()");
-
-            try
-            {
-                using (ChannelFactory<IFleetManagerService_PublicAPI_v2_0> channelFactory = CreateChannelFactory())
-                {
-                    IFleetManagerService_PublicAPI_v2_0 channel = channelFactory.CreateChannel();
-                    ServiceCallResultDto result = channel.ResetKingpin(ipAddress);
-                    channelFactory.Close();
-
-                    return result;
-                }
-            }
-            catch (Exception ex)
-            {
-                Logger.Error(ex);
-                return ServiceCallResultFactory.FromClientException(ex);
-            }
-        }
-
-        public IServiceCallResult SetFleetState(VehicleControllerState controllerState)
-        {
-            Logger.Trace("SetFleetState");
-
-            try
-            {
-                using (ChannelFactory<IFleetManagerService_PublicAPI_v2_0> channelFactory = CreateChannelFactory())
-                {
-                    IFleetManagerService_PublicAPI_v2_0 channel = channelFactory.CreateChannel();
-                    ServiceCallResultDto result = channel.SetFleetState(controllerState);
-                    channelFactory.Close();
-
-                    return result;
-                }
-            }
-            catch (Exception ex)
-            {
-                Logger.Error(ex);
-                return ServiceCallResultFactory.FromClientException(ex);
-            }
-        }
-
-        public IServiceCallResult SetKingpinState(IPAddress ipAddress, VehicleControllerState controllerState)
-        {
-            Logger.Trace("SetKingpinState");
-
-            try
-            {
-                using (ChannelFactory<IFleetManagerService_PublicAPI_v2_0> channelFactory = CreateChannelFactory())
-                {
-                    IFleetManagerService_PublicAPI_v2_0 channel = channelFactory.CreateChannel();
-                    ServiceCallResultDto result = channel.SetKingpinState(ipAddress, controllerState);
-                    channelFactory.Close();
-
-                    return result;
-                }
-            }
-            catch (Exception ex)
-            {
-                Logger.Error(ex);
-                return ServiceCallResultFactory.FromClientException(ex);
-            }
-        }
-
-        protected override void Dispose(bool isDisposing)
-        {
-            Logger.Debug($"Dispose isDisposing:{isDisposing}");
-
-            if (isDisposed) 
-                return;
-
-            if (isDisposing)            
-               callback.FleetStateUpdate -= Callback_FleetStateUpdate;
-            
-            base.Dispose(isDisposing);
-
-            isDisposed = true;
-        }
-
-        public IEnumerable<KingpinStateMailbox> GetModels() => kingpinStateMailboxes.ToList();
     }
 }
