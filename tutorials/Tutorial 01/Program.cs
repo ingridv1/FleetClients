c3caa0310e0f40e2eb5bebdc167e446c6c4824ca
Martin Davies <martin.davies@guidanceautomation.com>
2020-08-27 12:24:26 +0100

    Adds a very simple console app to do set position

---
+++ tutorials/Tutorial 01/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FleetClients.Core;
+using FleetClients.Core.FleetManagerServiceReference;
+using BaseClients.Core;
+using System.Net;
+using FleetClients.Core.Client_Interfaces;
+using GAAPICommon.Architecture;
+
+namespace Tutorial_01
+{
+    /// <summary>
+    /// A simple tutorial that shows how to create a virtual vehicle and reposition it:
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Here we create an endpoint settings object that defines where the fleet manager service is currently running
+            // For this demo we are assuming it is running on localhost, using the default TCP port of 41917.
+            EndpointSettings endpointSettings = new EndpointSettings(IPAddress.Loopback, 41916, 41917);
+
+            // Now we create a fleet manager client using the client factory;
+            IFleetManagerClient fleetManagerClient = ClientFactory.CreateTcpFleetManagerClient(endpointSettings);
+
+
+            Console.WriteLine("Press <any> key to create a virtual vehicle 192.168.0.1 at 0,0,0");
+            Console.ReadKey(true);
+
+            IPAddress virtualVehicle = IPAddress.Parse("192.168.0.1");
+            IServiceCallResult result = fleetManagerClient.CreateVirtualVehicle(virtualVehicle, 0, 0, 0);
+            if (!result.IsSuccessful())
+                throw new Exception(result.ToString());
+
+            // Now we can update the vehicles pose
+            Console.WriteLine("Press <any> key to set the pose of the vehicle to 1,1,1.57");
+            Console.ReadKey(true);
+
+            result = fleetManagerClient.SetPose(virtualVehicle, 1, 1, 1.57);
+            if (!result.IsSuccessful())
+                throw new Exception(result.ToString());
+
+            Console.WriteLine("Press <any> key to quit");
+            Console.ReadKey(true);
+
+            // The fleet manager client has its own thread which must be disposed.
+            fleetManagerClient.Dispose();
+        }
+    }
+}
---
+++ tutorials/Tutorial 02/Program.cs
@@ -0,0 +1,79 @@
+﻿using BaseClients.Core;
+using FleetClients.Core;
+using FleetClients.Core.Client_Interfaces;
+using FleetClients.Core.FleetManagerServiceReference;
+using GAAPICommon.Architecture;
+using GACore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Tutorial_02
+{
+    /// <summary>
+    /// A simple tutorial that creates a virtual vehicle and shows how to listen to fleet state updates
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Here we create an endpoint settings object that defines where the fleet manager service is currently running
+            // For this demo we are assuming it is running on localhost, using the default TCP port of 41917.
+            EndpointSettings endpointSettings = new EndpointSettings(IPAddress.Loopback, 41916, 41917);
+
+            // Now we create a fleet manager client using the client factory;
+            IFleetManagerClient fleetManagerClient = ClientFactory.CreateTcpFleetManagerClient(endpointSettings);
+
+            Console.WriteLine("Press <any> key to create a virtual vehicle 192.168.0.1 at 0,0,0");
+            Console.ReadKey(true);
+
+            IPAddress virtualVehicle = IPAddress.Parse("192.168.0.1");
+            IServiceCallResult result = fleetManagerClient.CreateVirtualVehicle(virtualVehicle, 0, 0, 0);
+            if (!result.IsSuccessful())
+                throw new Exception(result.ToString());
+
+            // Now we can subscribe to new fleet updates
+            Console.WriteLine("Press <any> key to subscribe to fleet updates");
+            Console.ReadKey(true);
+
+            Console.Clear();
+            Console.CursorVisible = false;
+            Console.WriteLine("Press <any> key to quit");
+
+            fleetManagerClient.FleetStateUpdated += FleetManagerClient_FleetStateUpdated;
+
+            Console.ReadKey(true);
+
+            fleetManagerClient.FleetStateUpdated -= FleetManagerClient_FleetStateUpdated;
+
+            // The fleet manager client has its own thread which must be disposed.
+            fleetManagerClient.Dispose();
+        }
+
+        private static void FleetManagerClient_FleetStateUpdated(FleetState fleetState)
+        {
+            if (fleetState == null)
+                throw new ArgumentNullException("fleetState");
+
+            Console.SetCursorPosition(0, 5);       
+
+            for (int i = 0; i < fleetState.KingpinStates.Count(); i++)
+            {
+                Console.Write(new String(' ', Console.BufferWidth));
+            }
+
+            Console.SetCursorPosition(0, 5);
+
+            Console.WriteLine($"Fleet size: {fleetState.KingpinStates.Count()}");
+
+            foreach (KingpinStateData kingpinState in fleetState.KingpinStates)
+            {
+                Console.WriteLine($"IP Address:{kingpinState.IPAddress} Tick:{kingpinState.Tick}, X:{kingpinState.X}, Y:{kingpinState.Y},  Heading:{kingpinState.Heading}");
+            }
+        }
+    }
+}
