be0b4fb68dfafbb246948a867daf63a9cd9c4aa5
Martin Davies <martin.davies@guidanceautomation.com>
2020-08-27 13:51:18 +0100

    Update Program.cs
    Using tidy

--- tutorials/Tutorial 02/Program.cs
+++ tutorials/Tutorial 02/Program.cs
@@ -5,12 +5,8 @@ using FleetClients.Core.FleetManagerServiceReference;
 using GAAPICommon.Architecture;
 using GACore;
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Net;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Input;
 
 namespace Tutorial_02
 {
